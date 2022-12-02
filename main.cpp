737aa6ae81d3f6499c8ff56a06985010b9efa113
RGioele <116067942+RGioele@users.noreply.github.com>
2022-12-02 16:50:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int main(){
+  int h;
+  cin>>h;
+  if (h > 0){
+      cout<<"numero primo";
+  }
+  else{
+  cout<<"numero non primo";
+
+  }
   return 0;
 }
